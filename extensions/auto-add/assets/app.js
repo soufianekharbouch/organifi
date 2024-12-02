e81290ccf3d2ed4a82a1d88edabd48db862f46c6
jacqueline <jacqueline.jones-ic@olaplex.com>
2024-12-02 23:06:07 +0100

    mlmmm

--- extensions/auto-add/assets/app.js
+++ extensions/auto-add/assets/app.js
@@ -1,25 +1,23 @@
-// Fonction pour récupérer les promos depuis l'API
+
 async function fetchPromos() {
     try {
-      // URL mise à jour pour l'API
-      const response = await fetch("https://ir-clothing-so-respond.trycloudflare.com/api/promos");
+
+      const response = await fetch("https://organifi-1a031727f073.herokuapp.com/api/promos");
   
       if (!response.ok) {
         throw new Error("Erreur lors de la récupération des promos.");
       }
-  
-      // Convertir la réponse en JSON
+
       const promos = await response.json();
-  
-      // Parcourir les promos et afficher les IDs dans des alertes
+
       promos.forEach((promo) => {
         alert(`Promo: ${promo.title}
   Target Product ID: ${promo.targetProductId}
   Gift Product ID: ${promo.giftProductId}`);
       });
     } catch (error) {
-      console.error("Erreur :", error);
-      alert("Une erreur est survenue lors de la récupération des promos.");
+      console.error("ERROR :", error);
+      alert("ERROR.");
     }
   }
   
